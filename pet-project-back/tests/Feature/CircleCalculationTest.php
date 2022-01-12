e8e2d2a6634d64cb32e5cc4f4ca29479998fe996
Vitalii Svietkov <vitalii.svietkov>
2022-01-12 21:48:09 +0200

    Extended the Circle feature test

--- pet-project-back/tests/Feature/CircleCalculationTest.php
+++ pet-project-back/tests/Feature/CircleCalculationTest.php
@@ -6,6 +6,9 @@ use Tests\BaseShapeFeatureTest;
 
 class CircleCalculationTest extends BaseShapeFeatureTest
 {
+    /**
+     * @return void
+     */
     protected function setUp(): void
     {
         parent::setUp();
@@ -20,6 +23,8 @@ class CircleCalculationTest extends BaseShapeFeatureTest
 
     /**
      * Test the request for circle data calculation
+     *
+     * @return void
      */
     public function test__circleCalculationRequest()
     {
@@ -29,22 +34,48 @@ class CircleCalculationTest extends BaseShapeFeatureTest
         $response->assertStatus(422);
 
         $response = $this->postJson($this->endpoint, [
-            'shape'  => 'Circle',
-            'radius' => 1,
-        ]);
-        $response->assertExactJson($this->expectedJsonResult);
-
-        $response = $this->postJson($this->endpoint, [
             'shape'           => 'Circle',
-            'radius'          => 1,
+            'radius'          => $this->expectedJsonResult['radius'],
             'Circle_diameter' => 3,
         ]);
         $response->assertStatus(422);
 
         $response = $this->postJson($this->endpoint, [
             'shape'           => 'Circle',
-            'radius'          => 1,
-            'Circle_diameter' => 2,
+            'radius'          => $this->expectedJsonResult['radius'],
+            'Circle_diameter' => $this->expectedJsonResult['diameter'],
+        ]);
+        $response->assertExactJson($this->expectedJsonResult);
+
+        $this->successCalculationsWithSingleInput();
+    }
+
+    /**
+     * @return void
+     */
+    private function successCalculationsWithSingleInput()
+    {
+        $response = $this->postJson($this->endpoint, [
+            'shape'  => 'Circle',
+            'radius' => $this->expectedJsonResult['radius'],
+        ]);
+        $response->assertExactJson($this->expectedJsonResult);
+
+        $response = $this->postJson($this->endpoint, [
+            'shape'           => 'Circle',
+            'Circle_diameter' => $this->expectedJsonResult['diameter'],
+        ]);
+        $response->assertExactJson($this->expectedJsonResult);
+
+        $response = $this->postJson($this->endpoint, [
+            'shape'       => 'Circle',
+            'Circle_area' => $this->expectedJsonResult['area'],
+        ]);
+        $response->assertExactJson($this->expectedJsonResult);
+
+        $response = $this->postJson($this->endpoint, [
+            'shape'            => 'Circle',
+            'Circle_perimeter' => $this->expectedJsonResult['perimeter'],
         ]);
         $response->assertExactJson($this->expectedJsonResult);
     }
