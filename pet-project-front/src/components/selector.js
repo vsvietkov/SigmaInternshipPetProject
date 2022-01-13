5d77465975a43fb8218118081a589a554c9571fd
Vitalii Svietkov <vitalii.svietkov>
2022-01-13 22:32:40 +0200

    #15 Removed unnecessary inputs

--- pet-project-front/src/components/selector.js
+++ pet-project-front/src/components/selector.js
@@ -11,17 +11,10 @@ export function Selector(props) {
             >
                 <option disabled>2D figures</option>
                 <option defaultValue="circle">Circle</option>
-                <option defaultValue="triangle">Triangle</option>
-                <option defaultValue="quadrangle">Quadrangle</option>
                 <option defaultValue="square">Square</option>
-                <option defaultValue="rectangle">Rectangle</option>
-                <option defaultValue="trapezoid">Trapezoid</option>
-                <option defaultValue="parallelogram">Parallelogram</option>
                 <option disabled>3D figures</option>
                 <option defaultValue="sphere">Sphere</option>
-                <option defaultValue="cylinder">Cylinder</option>
                 <option defaultValue="cube">Cube</option>
-                <option defaultValue="parallelepiped">Parallelepiped</option>
             </select>
             <span id="shape-error" className="text-danger input-error px-1"></span>
         </div>
--- pet-project-front/src/components/tools/helpers.js
+++ pet-project-front/src/components/tools/helpers.js
@@ -2,23 +2,11 @@ import {CircleInputs} from "../figureInputs/2D/circleInputs";
 import {Inputs2D} from "../figureInputs/default2Dinputs";
 import {Inputs3D} from "../figureInputs/default3Dinputs";
 import {SphereInputs} from "../figureInputs/3D/sphereInputs";
-import {CylinderInputs} from "../figureInputs/3D/cylinderInputs";
 import {CircleImage} from "../figureImages/circleImage";
 import {SphereImage} from "../figureImages/sphereImage";
-import {CylinderImage} from "../figureImages/cylinderImage";
 import {CubeImage} from "../figureImages/cubeImage";
-import {ParallelepipedImage} from "../figureImages/parallelepipedImage";
-import {TriangleImage} from "../figureImages/triangleImage";
-import {QuadrangleImage} from "../figureImages/quadrangleImage";
-import {RectangleImage} from "../figureImages/rectangleImage";
 import {SquareImage} from "../figureImages/squareImage";
-import {TrapezoidImage} from "../figureImages/trapezoidImage";
-import {ParallelogramImage} from "../figureImages/parallelogramImage";
 import {SquareInputs} from "../figureInputs/2D/squareInputs";
-import {RectangleInputs} from "../figureInputs/2D/rectangleInputs";
-import {TrapezoidInputs} from "../figureInputs/2D/trapezoidInputs";
-import {TriangleInputs} from "../figureInputs/2D/triangleInputs";
-import {ParallelogramInputs} from "../figureInputs/2D/parallelogramInputs";
 import {CubeInputs} from "../figureInputs/3D/cubeInputs";
 
 export function clearErrorSpans() {
@@ -31,26 +19,12 @@ export function getFigureImage(figureName) {
   switch (figureName) {
     case 'Circle':
       return <CircleImage />
-    case 'Triangle':
-      return <TriangleImage />
-    case 'Quadrangle':
-      return <QuadrangleImage />
     case 'Square':
       return <SquareImage />
-    case 'Rectangle':
-      return <RectangleImage />
-    case 'Trapezoid':
-      return <TrapezoidImage />
-    case 'Parallelogram':
-      return <ParallelogramImage />
     case 'Sphere':
       return <SphereImage />
-    case 'Cylinder':
-      return <CylinderImage />
     case 'Cube':
       return <CubeImage />
-    case 'Parallelepiped':
-      return <ParallelepipedImage />
     default:
       return <></>
   }
@@ -60,20 +34,10 @@ export function getFigureInputs(figureName) {
   switch (figureName) {
     case 'Circle':
       return <CircleInputs />
-    case 'Triangle':
-      return <TriangleInputs />
     case 'Square':
       return <SquareInputs />
-    case 'Rectangle':
-      return <RectangleInputs />
-    case 'Trapezoid':
-      return <TrapezoidInputs />
-    case 'Parallelogram':
-      return <ParallelogramInputs />
     case 'Sphere':
       return <SphereInputs />
-    case 'Cylinder':
-      return <CylinderInputs />
     case 'Cube':
       return <CubeInputs />
     default:
@@ -93,17 +57,10 @@ export function getDefaultInputs(currentFigureName) {
 
 export const FIGURES_2D = [
   'Circle',
-  'Triangle',
-  'Quadrangle',
   'Square',
-  'Rectangle',
-  'Trapezoid',
-  'Parallelogram',
 ]
 
 export const FIGURES_3D = [
   'Sphere',
-  'Cylinder',
   'Cube',
-  'Parallelepiped',
 ]
